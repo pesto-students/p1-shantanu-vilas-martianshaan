dde23c54793e531e00650e0d7166b004fcd60d91
martianshaan <shantanu.kale@viit.ac.in>
2022-05-08 17:01:25 +0530

    week-1

---
+++ Week-3/3.2 call blind apply/index.js
@@ -0,0 +1,15 @@
+let fruits = {
+    name: 'Mango',
+    color: 'yellow'
+}
+
+const fruitsInfo= function(origin,use) {
+    console.log(this.name + " "+ "of" + " " + this.color +" "+"color is my favorite fruit from" +" "+ origin + " "+ 
+    "for" + " "+ use);
+}
+fruitsInfo.call(fruits,'kokan','juice');
+
+fruitsInfo.apply(fruits,['kokan','juice']);
+
+const food = fruitsInfo.bind(fruits,'Kokan','juice');
+food();
---
+++ Week-4/4.2 prototype/index.js
@@ -0,0 +1,9 @@
+var Person = function() {};
+Person.prototype.initialize= function(name,age)
+{
+    this.name = name;
+    this.age= age;
+    
+}
+
+var him = new Teacher();
